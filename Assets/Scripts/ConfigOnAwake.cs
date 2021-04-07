1dbc765a356ebed2c2b8d4c5e819cb9372605104
Paternostro <pedro.paternostro@gmail.com>
2021-04-07 13:39:14 -0300

    Removed teleportation and added collisions
    Also made a scene starting in the simulation mode

---
+++ Assets/Scripts/ConfigOnAwake.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ConfigOnAwake : MonoBehaviour
+{
+    public UnityEvent DoOnAwake;
+
+    private void Awake()
+    {
+        DoOnAwake.Invoke();
+    }
+}
